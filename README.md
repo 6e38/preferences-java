137d9b2faba1c185296845158f65361034fb3452
6e38 <6e38@floorsix.com>
2018-09-08 06:38:32 -0600

    Adding json submodule

---
+++ README.md
@@ -0,0 +1,20 @@
+# Preferences
+*library for java*
+
+Java has a built-in preferences class and I really wanted to use it, but for some reason they don't create the appropriate registry key for Windows thus making it useless. Much sadness.
+
+This library comes with no warranty. Use at your own risk.
+
+You may use and/or modify this code.
+
+## Known Issues
+* Depends on a super lite json library
+
+## Usage
+
+`ant compile` builds the project
+
+`ant test` runs some tests on the code
+
+`ant jar` packages the project
+
--- src/com/floorsix/preferences/Preferences.java
+++ src/com/floorsix/preferences/Preferences.java
@@ -1,7 +1,11 @@
 
 package com.floorsix.preferences;
 
+import com.floorsix.json.*;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class Preferences
 {
@@ -28,8 +32,38 @@ public class Preferences
     return new Preferences(prefsPath, filename);
   }
 
+  private File f;
+  private JsonObject root;
+
   private Preferences(String path, String filename)
   {
+    f = new File(path + File.separator + filename);
+
+    if (f.exists())
+    {
+      try
+      {
+        root = JsonParser.parse(new FileInputStream(f));
+
+        System.out.println("Read file: " + root);
+      }
+      catch (InvalidJsonException e)
+      {
+        root = new JsonObject(null);
+
+        System.out.println("Could not read file: " + root);
+      }
+      catch (FileNotFoundException e)
+      {
+        root = new JsonObject(null);
+      }
+    }
+    else
+    {
+      root = new JsonObject(null);
+
+      System.out.println("No file: " + root);
+    }
   }
 
   public void commit()
@@ -46,10 +80,12 @@ public class Preferences
 
   public String get(String key)
   {
+    return null;
   }
 
   public double getDouble(String key)
   {
+    return 0;
   }
 }
 
