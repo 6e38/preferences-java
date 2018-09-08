b4e25c86e7db9f95519c1588de58fffaee20f511
6e38 <6e38@floorsix.com>
2018-09-08 10:26:35 -0600

    Finishing basic functionality for preferences

--- src/com/floorsix/preferences/Preferences.java
+++ src/com/floorsix/preferences/Preferences.java
@@ -5,14 +5,18 @@ import com.floorsix.json.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.InputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Preferences
 {
-  static final String prefsFolder = ".java-user-prefs";
-  static final String prefsSuffix = ".json";
+  private static final String prefsFolder = ".java-user-prefs";
+  private static final String prefsSuffix = ".json";
 
-  static Preferences getUserPreferences(Class c)
+  public static Preferences getUserPreferences(Class c)
   {
     String className = c.getName();
 
@@ -43,15 +47,21 @@ public class Preferences
     {
       try
       {
-        root = JsonParser.parse(new FileInputStream(f));
+        FileInputStream in = new FileInputStream(f);
+        root = JsonParser.parse(in);
 
-        System.out.println("Read file: " + root);
+        try
+        {
+          in.close();
+        }
+        catch (IOException e)
+        {
+          // failed to close, ignore
+        }
       }
       catch (InvalidJsonException e)
       {
         root = new JsonObject(null);
-
-        System.out.println("Could not read file: " + root);
       }
       catch (FileNotFoundException e)
       {
@@ -61,31 +71,112 @@ public class Preferences
     else
     {
       root = new JsonObject(null);
-
-      System.out.println("No file: " + root);
     }
   }
 
-  public void commit()
+  public boolean delete()
   {
+    boolean success;
+
+    success = f.delete();
+
+    return success;
   }
 
-  public void set(String key, String value)
+  public boolean commit()
   {
+    boolean success;
+
+    try
+    {
+      FileWriter out = new FileWriter(f);
+      String json = root.toJson();
+      out.write(json, 0, json.length());
+      out.close();
+      success = true;
+    }
+    catch (IOException e)
+    {
+      success = false;
+    }
+
+    return success;
   }
 
-  public void set(String key, double value)
+  public void setString(String key, String value)
   {
+    root.set(key, value);
   }
 
-  public String get(String key)
+  public void setDouble(String key, double value)
   {
-    return null;
+    root.set(key, value);
   }
 
-  public double getDouble(String key)
+  public void setList(String key, List<String> value)
   {
-    return 0;
+    JsonArray array = root.setArray(key);
+
+    for (String s : value)
+    {
+      array.add(s);
+    }
+  }
+
+  public String getString(String key, String defaultValue)
+  {
+    Json json = root.get(key);
+
+    if (json instanceof JsonString)
+    {
+      return ((JsonString)json).get();
+    }
+    else
+    {
+      return defaultValue;
+    }
+  }
+
+  public double getDouble(String key, double defaultValue)
+  {
+    Json json = root.get(key);
+
+    if (json instanceof JsonNumber)
+    {
+      return ((JsonNumber)json).get();
+    }
+    else
+    {
+      return defaultValue;
+    }
+  }
+
+  public List<String> getList(String key)
+  {
+    ArrayList<String> list = new ArrayList<String>();
+
+    Json json = root.get(key);
+
+    if (json instanceof JsonArray)
+    {
+      List<Json> l = ((JsonArray)json).getArray();
+
+      for (Json j : l)
+      {
+        if (j instanceof JsonString)
+        {
+          list.add(((JsonString)j).get());
+        }
+      }
+    }
+
+    return list;
+  }
+
+  @Override
+  public String toString()
+  {
+    return root.toJson();
   }
 }
 
--- src/com/floorsix/preferences/Test.java
+++ src/com/floorsix/preferences/Test.java
@@ -1,11 +1,89 @@
 
 package com.floorsix.preferences;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test
 {
   public static void main(String[] args)
   {
+    try
+    {
+      test();
+      System.out.println("\nPASS: all tests succeeded");
+    }
+    catch (AssertionError e)
+    {
+      System.out.print("\nTest failure");
+
+      if (e.getMessage() != null)
+      {
+        System.out.print(": " + e.getMessage());
+      }
+
+      System.out.println();
+
+      for (StackTraceElement trace : e.getStackTrace())
+      {
+        System.out.println(">> " + trace);
+      }
+
+      System.out.println("\nFAIL");
+    }
+  }
+
+  private static void test()
+  {
     Preferences p = Preferences.getUserPreferences(Test.class);
+    p.delete();
+
+    p = Preferences.getUserPreferences(Test.class);
+    assert p.toString().equals("{}");
+    p.commit();
+
+    p = Preferences.getUserPreferences(Test.class);
+    assert p.toString().equals("{}");
+    p.setString("k1", "s1");
+    assert p.toString().equals("{\n\"k1\": \"s1\"\n}");
+    p.commit();
+
+    p = Preferences.getUserPreferences(Test.class);
+    assert p.getString("k1", "x").equals("s1");
+    assert p.getString("k2", "x").equals("x");
+    p.setString("k1", "s3");
+    p.setString("k2", "s2");
+    p.commit();
+
+    p = Preferences.getUserPreferences(Test.class);
+    assert p.getString("k1", "x").equals("s3");
+    assert p.getString("k2", "x").equals("s2");
+    p.setDouble("k3", 3);
+    p.setDouble("k1", 1);
+    p.commit();
+
+    p = Preferences.getUserPreferences(Test.class);
+    assert p.getString("k1", "x").equals("x");
+    assert p.getString("k2", "x").equals("s2");
+    assert (int)p.getDouble("k1", -1) == 1;
+    assert (int)p.getDouble("k3", -1) == 3;
+    assert (int)p.getDouble("k2", -1) == -1;
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("s1");
+    list.add("s2");
+    list.add("s3");
+    p.setList("k4", list);
+    p.commit();
+
+    p = Preferences.getUserPreferences(Test.class);
+    List<String> l = p.getList("k4");
+    assert l != null;
+    assert p.getList("k5") != null;
+    assert p.getList("k5").size() == 0;
+    assert l.size() == 3;
+    assert l.get(0).equals("s1");
+    assert l.get(1).equals("s2");
+    assert l.get(2).equals("s3");
   }
 }
 
