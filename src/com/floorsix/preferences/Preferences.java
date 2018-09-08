c6e7ddea48daa64103bbca9f02e9e2e1ea92a1f6
6e38 <6e38@floorsix.com>
2018-09-08 10:41:24 -0600

    First release

--- src/com/floorsix/preferences/Preferences.java
+++ src/com/floorsix/preferences/Preferences.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.InputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
